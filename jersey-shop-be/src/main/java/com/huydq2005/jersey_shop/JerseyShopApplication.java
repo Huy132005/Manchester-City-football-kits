8cc2030c687616020e8a0a77f0c06bec6d8646e1
Đỗ Quang Huy <149219260+Huy132005@users.noreply.github.com>
2026-02-14 16:18:56 +0700

    Manchester City football kits

---
+++ jersey-shop-be/src/main/java/com/huydq2005/jersey_shop/JerseyShopApplication.java
@@ -0,0 +1,13 @@
+package com.huydq2005.jersey_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JerseyShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JerseyShopApplication.class, args);
+	}
+
+}
---
+++ jersey-shop-be/src/test/java/com/huydq2005/jersey_shop/JerseyShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huydq2005.jersey_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JerseyShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
